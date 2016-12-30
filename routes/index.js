8f9fbf81f00f29c8507fee05b19f03da13e410b0
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-12-30 02:41:45 -0500

    s3 config

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 var homeController = require('../controllers/main'),
-    movieCreate    = require('../controllers/movie_create'),
-    moviesByName   = require('../controllers/movie_get_all_by_name'),
-    movieByName    = require('../controllers/movie_get_one_by_name');
+    movieCreate = require('../controllers/movie_create'),
+    moviesByName = require('../controllers/movie_get_all_by_name'),
+    movieByName = require('../controllers/movie_get_one_by_name');
+    signS3 = require('../controllers/s3');
 
 
-  // index route
-  router.get('/', homeController);
-  router.get('/test', function (req, res) {
-    console.log(req);
+// index route
+router.get('/', homeController);
+router.get('/test', function (req, res) {
+  console.log(req);
+});
+
+// movie routes
+router.post('/movie', movieCreate);
+router.get('/movies', moviesByName);
+router.get('/movie/:title', movieByName);
+
+// s3 signed url
+router.get('/signS3', function (req, res) {
+  signS3(req.query, function (signedRequest, err) {
+    if (err) res.end();
+
+    res.write(JSON.stringify(signedRequest));
+    res.end();
   });
-
-  // movie routes
-  router.post('/movie', movieCreate);
-  router.get('/movie', moviesByName);
-  router.get('/movie/:title', movieByName);
+});
 
 module.exports = router;
