c9d0eabd1fa5d6b8ec2cd42f02d49a831bd8761e
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2016-12-30 21:33:07 -0500

    get reviews and average rating

--- controllers/movie_get_one_by_name.js
+++ controllers/movie_get_one_by_name.js
@@ -1,25 +1,52 @@
 module.exports = function (req, res) {
-  var title = req.params.title.replace('-',' ');
-  console.log(title);
-  db.Movie.findOne({ title: title })
-      .then(function (movieData) {
-        // remove ids and timestamps
-        var movie = {
-            title: movieData.title,
-            summery: movieData.summery,
-            coverURL: movieData.coverURL
-          };
+  var title = req.params.title.replace(/-/g, ' ').trim().toLowerCase();
 
-        res.json({
-          status: 'ok',
-          movie: movie
+  db.Movie.findOne({title: title})
+
+    .then(getReviews)
+
+    .catch(function (err) {
+      console.log(err);
+      res.json({
+        status: 'failed',
+        msg: 'could not find movie'
+      });
+    });
+
+  function getReviews(movieData) {
+    db.Review.findAll({where: {movieId: movieData.id}})
+      // get average rating
+      .then(function (reviews) {
+
+        var totalReviews = reviews.length;
+        var sum = reviews.reduce(function (a, b) {
+          return a.rating + b.rating;
+        });
+
+        var movie = movieData;
+        movie.avgRating = sum / totalReviews;
+        return {
+          movie: movie,
+          reviews: reviews
+        }
+      })
+      // render movie and reviews
+      .then(function (movieReviewData) {
+        res.render('movie', {
+          coverURL: movieReviewData.movie.coverURL,
+          title: movieReviewData.movie.title,
+          summary: movieReviewData.movie.summary,
+          isLoggedIn: req.isAuthenticated(),
+          avgRating: movieReviewData.movie.avgRating,
+          reviews: movieReviewData.reviews
         });
       })
       .catch(function (err) {
         console.log(err);
         res.json({
           status: 'failed',
-          msg: 'could not find movie'
+          msg: 'could not find reviews'
         })
       })
+  }
 };
