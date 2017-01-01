b40a1edb05f937620ef9c61bcd026ae04cb38003
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-01-01 15:49:57 -0500

    review route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var homeController = require('../controllers/main'),
-    movieCreate = require('../controllers/movie_create'),
-    moviesByName = require('../controllers/movie_get_all_by_name'),
-    movieByName = require('../controllers/movie_get_one_by_name');
-    signS3 = require('../controllers/s3');
+    movieCreate    = require('../controllers/movie_create'),
+    moviesByName   = require('../controllers/movie_get_all_by_name'),
+    movieByName    = require('../controllers/movie_get_one_by_name'),
+    reviewCreate   = require('../controllers/review_create');
+    signS3         = require('../controllers/s3');
 
 
 // index route
@@ -18,6 +19,9 @@ router.get('/test', function (req, res) {
 router.post('/movie', movieCreate);
 router.get('/movies', moviesByName);
 router.get('/movie/:title', movieByName);
+
+// review routes
+router.post('/review', reviewCreate);
 
 // s3 signed url
 router.get('/signS3', function (req, res) {
