7d4ae01bca2de6da38e3b67a044627ae36e59a72
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-01-01 15:46:26 -0500

    admin column name fix :satisfied:

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    admim: {
+    admin: {
       type: DataTypes.BOOLEAN,
       default: 0
     }
