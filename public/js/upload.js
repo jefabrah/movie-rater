1f2b4e23d804ad53a92b7ae7dcf07c9a6ab6d66c
Jeffrey Abraham <jeffreyabraham64@gmail.com>
2017-01-02 19:29:47 -0500

    remove es6 syntax and use jquery

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,55 +1,67 @@
-function uploadFile(file, signedRequest, url, fileType) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('PUT', signedRequest);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
+($(document).ready(function () {
+  /*
+   Function to carry out the actual PUT request to S3 using the signed request from the app.
+   */
+  function uploadFile(file, signedRequest, url){
 
-          document.getElementById('picture-url').value = url;
-
-      } else {
-        alert('Could not upload file.');
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', signedRequest);
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState === 4){
+        if(xhr.status === 200){
+          $('#preview').attr('src', url);
+          $('#coverurl-input').attr('value', url);
+        }
+        else{
+          alert('Could not upload file.');
+        }
       }
-    }
-  };
-  xhr.send(file);
-}
-/*
- Function to get the temporary signed request from the app.
- If request successful, continue to upload the file using this signed
- request.
- */
-function getSignedRequest(file, fileType) {
-  var uniqueFileName = Date.now();
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/signS3?file-name=' + uniqueFileName + '_' + file.name + '&file-type=' + file.type);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        uploadFile(file, response.signedRequest, response.url, fileType);
-      } else {
-        alert('Could not get signed URL.');
-      }
-    }
-  };
-  xhr.send();
-}
-/*
- Function called when file input updated. If there is a file selected, then
- start upload procedure by asking for a signed request from the app.
- */
-function initPicUpload() {
-  var files = document.getElementById('cover-input').files;
-  var file = files[0];
-  if (file == null) {
-    return alert('No file selected.');
+    };
+    xhr.send(file);
   }
-  getSignedRequest(file, 'picture');
-}
+
+  /*
+   Function to get the temporary signed request from the app.
+   If request successful, continue to upload the file using this signed
+   request.
+   */
+  function getSignedRequest(file){
+    // check for file image file extension
+    if (!file.name.match(/\.(jpg|jpeg|png|gif)$/)) {
+      alert('File needs to be an image');
+      console.log(file.type);
+      return;
+    }
+
+    // generate random file suffix
+    var date = new Date();
+    var fileSuffix = parseInt(date.getTime() / Math.random() * (100 - 1) + 100);
+
+    $.get('/signS3?file-name='+fileSuffix+file.name+'&file-type='+file.type,
+    function (resData) {
+      console.log(resData);
+      var response = JSON.parse(resData);
+      uploadFile(file, response.signedRequest, response.url);
+    })
+      .fail(function () {
+        alert('Could not get signed URL for AWS s3');
+      });
+  }
+
+  /*
+   Function called when file input updated. If there is a file selected, then
+   start upload procedure by asking for a signed request from the app.
+   */
+  function initUpload(){
+    const files = document.getElementById('cover-input').files;
+    const file = files[0];
+    if(file == null){
+      return alert('No file selected.');
+    }
+    getSignedRequest(file);
+  }
 
 
-// Bind listeners when the page loads.
-(function () {
-  document.getElementById('cover-input').onchange = initPicUpload;
-})();
+  // bind event listener
+  document.getElementById('cover-input').onchange = initUpload;
+}));
